312c939a68007126d9646c84ef957e18d0f704ac
Andy Place <info@andyplace.co.uk>
2013-06-05 13:06:47 +0100

    Version Control

---
+++ add-ons/gateways/gateway.realmpi.js
@@ -0,0 +1,14 @@
+//add sagepay redirection
+$(document).bind('em_booking_gateway_add_realex_remote', function(event, response){
+
+	// called by EM if return JSON contains gateway key, notifications messages are shown by now.
+	if(response.result){
+		var spForm = $('<form action="'+response.realmpi_url+'" method="post" id="em-sagepay-form-redirect-form"></form>');
+		$.each( response.realmpi_form, function(index,value){
+			spForm.append('<input type="hidden" name="'+index+'" value="'+value+'" />');
+		});
+		spForm.append('<input id="em-realmpi-submit" type="submit" style="display:none" />');
+		spForm.insertAfter('#em-booking-form').trigger('submit');
+	}
+	
+});
---
+++ events-manager-pro-realex-remote.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Events Manager Pro - RealEx Remote Gateway
+Plugin URI: http://wp-events-plugin.com
+Description: RealEx Remote payment gateway pluging for Events Manager Pro
+Version: 1.2
+Depends: Events Manager Pro
+Author: Andy Place
+Author URI: http://www.andyplace.co.uk
+
+Change Log:
+
+1.2     Tooltip added for CVV number - 24/03/2013
+
+1.1 	Updated to work with EM PRO 2.3 with multiple bookings - 13/03/2013
+		http://wp-events-plugin.com/documentation/multiple-booking-mode/
+
+1.0 	May 2012
+
+*/
+
+class EM_Pro_RealEx_Remote {
+
+	function EM_Pro_RealEx_Remote() {
+		global $wpdb;
+		//Set when to run the plugin : after EM is loaded.
+		add_action( 'plugins_loaded', array(&$this,'init'), 101 );
+	}
+
+	function init() {
+		//add-ons
+		include('add-ons/gateways/gateway.realex.remote.php');
+	}
+
+}
+
+// Start plugin
+global $EM_Pro_RealEx_Remote;
+$EM_Pro_RealEx_Remote = new EM_Pro_RealEx_Remote();
